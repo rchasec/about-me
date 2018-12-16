53208cb559f7c85edc5ac1d665810199c6e829c9
Chase Campbell <3chasecampbell@gmail.com>
2018-12-15 18:32:22 -0700

    converted html files into js files and cleaned up jsx

---
+++ src/AboutMePage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+export default class AboutMePage extends Component {
+    render = () => {
+        return(
+            <div>
+                <section>
+                    <h1>Ryan Chase Campbell</h1>
+                </section>
+                <section>
+                    <img src="C:\Users\Chase\Desktop\Helio\projects\images for projects/pizzapic.jpg" alt="picture of chase"/>
+                </section>
+                <section>
+                    <h3>Bio:</h3>
+                    <p>Chase attended Utah State University for Business Management in 2015.  He then attended Utah Valley University for Entreprenuership.</p>
+                </section>
+                <section>
+                    <h4>Why Helio</h4>
+                    <p>Because Helio is great</p>
+                </section>
+                <section>
+                    <h4>Other Info:</h4>
+                    <ul>
+                        <li>
+                            <a href="workexperience.html">My Work Experience</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/rchasec">GitHub</a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/chase-campbell-518234133/">LinkedIn</a>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <h4>Contact Me:</h4>
+                    <form>
+                        Name: <br/>
+                        <input type="text" /><br/>
+                        E-mail: <br/>
+                        <input type="text" /><br/>
+                        Request: <br/>
+                        <textarea rows="5" cols="35"></textarea><br/>
+                        <input type="submit" />
+                    </form>
+                </section> 
+            </div>
+        )
+    }
+}
---
+++ src/WorkExperiencePage.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+export default class WorkExperiencePage extends Component {
+    render = () => {
+        return (
+            <div>
+                <section>
+                    <h1>Chase's Work Experience</h1>
+                </section>
+                <div>
+                    <ul>
+                        <h2>
+                            <p>iBoom Media</p>
+                            <p>September 2017 - January 2017</p>
+                            <p>Social Media Assistant</p>
+                        </h2>
+                        <p>Roles:</p>
+                        <ul>
+                            <li>
+                                Oversee Twitter follower growth for all clients
+                            </li>
+                        </ul>
+                    </ul>
+                </div>
+                <section>
+                    <ul>
+                        <h2>
+                            <p>Results RNA</p>
+                            <p>January 2017 - August 2018</p>
+                            <p>Social Media Assistant</p>
+                        </h2>
+                        <p>Roles:</p>
+                            <ul>
+                                <li>Research topics used in social media content</li>
+                                <li>Design content</li>
+                                <li>Write content captions</li>
+                                <li>Check content for FDA compliance issues</li>
+                                <li>Oversee content publishing</li>
+                            </ul>
+                    </ul>
+                </section>
+                <div>
+                    <ul>
+                        <h2>
+                            <p>Arego International LLC</p>
+                            <p>September 2018 - Present</p>
+                            <p>Customer Service Manager/Marketing Content Designer</p>
+                        </h2>
+                        <p>Roles:</p>
+                        <ul>
+                            <li>Train and manage customer service agents</li>
+                            <li>Design Marketing Materials</li>
+                        </ul>
+                    </ul>
+                </div>
+                <section>
+                    <a href="aboutme.html">Go to home</a>
+                </section>
+            </div>
+        )
+    }
+}
